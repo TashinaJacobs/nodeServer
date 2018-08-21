a64f9c82bcd16f0bfe20fbc6e89559853f3e239a
tashina romana jacobs <845865220@bun2-206-23.local>
2018-08-22 08:46:05 +1200

    added more stuff

--- index.js
+++ index.js
@@ -1,33 +1,92 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
 
 var server = http.createServer(function(request, response){
-  var page;
-  if (request.url === "/") {
-    page = "home";
-  }else if (request.url === "/contact") {
-    page = "contact";
-  }else if (request.url === "/about") {
-    page = "about";
-  }else {
-    page = "404 page not found"
-  }
-  console.log(request);
-  response.writeHead(200, {'Content-Type':'text/html'});
-  response.end(`
-      <html>
-        <head>
-            <title>Node Server</title>
-        </head>
-        <body>
-            <h1>${page}</h1>
-            <p>${request.url}</p>
-            <p>${request.method}</p>
-        </body>
-      </html>
+    console.log(`${request.method} request for ${request.url}`);
+    var page;
 
+    if(request.url === "/" || request.url === "/home" || request.url === "/index"){
+        page = "home";
+        fs.readFile('./public/index.html', 'UTF-8', function(error, contents){
+            if(error){
+                console.log("error, something went wrong");
+            } else {
+                response.writeHead(200, {'Content-Type':'text/html'});
+                response.end(contents);
+            }
+        });
+    } else if(request.url === "/about"){
+        fs.readFile('./public/about.html', 'UTF-8', function(error, contents){
+            if(error){
+                console.log("error, something went wrong");
+            } else {
+                response.writeHead(200, {'Content-Type':'text/html'});
+                response.end(contents);
+            }
+        });
+    } else if(request.url === "/contact"){
+        fs.readFile('./public/contact.html', 'UTF-8', function(error, contents){
+            if(error){
+                console.log("error, something went wrong");
+            } else {
+                response.writeHead(200, {'Content-Type':'text/html'});
+                response.end(contents);
+            }
+        });
+    } else if(request.url === "/news"){
+        fs.readFile('./public/news.html', 'UTF-8', function(error, contents){
+            if(error){
+                console.log("error, something went wrong");
+            } else {
+                response.writeHead(200, {'Content-Type':'text/html'});
+                response.end(contents);
+            }
+        });
+    } else if(request.url.match(/.js/)){
+        var jsPath = path.join(__dirname, 'public', request.url);
+        var fileStream = fs.createReadStream(jsPath, 'UTF-8');
+        response.writeHead(200, {'Content-Type': 'text/javascript'});
+        fileStream.pipe(response);
+    } else if(request.url.match(/.css$/)){
+        var cssPath = path.join(__dirname, 'public', request.url);
+        var fileStream = fs.createReadStream(cssPath, 'UTF-8');
+        response.writeHead(200, {'Content-Type': 'text/css'});
+        fileStream.pipe(response);
+    } else if(request.url.match(/.jpg$/)){
+		var imagePath = path.join(__dirname, 'public', request.url);
+		var imageStream = fs.createReadStream(imagePath);
+		response.writeHead(200, {'Content-Type':'image/jpeg'});
+		imageStream.pipe(response);
+	} else if(request.url.match(/.png/)){
+		var imagePath = path.join(__dirname, 'public', request.url);
+		var imageStream = fs.createReadStream(imagePath);
+		response.writeHead(200, {'Content-Type':'image/png'});
+		imageStream.pipe(response);
+	}
 
+    // else if(request.url === "/contact"){
+    //     page = "contact";
+    // } else if(request.url === "/about"){
+    //     page = "about";
+    // } else {
+    //     page = "404 page not found";
+    // }
 
-    `);
+    // response.writeHead(200, {'Content-Type':'text/html'});
+    // response.end(`
+    //     <html>
+    //         <head>
+    //             <title>Node Server</title>
+    //         </head>
+    //         <body>
+    //             <h1>${page}</h1>
+    //             <p>${request.url}</p>
+    //             <p>${request.method}</p>
+    //         </body>
+    //     </html>
+    //     `);
 });
 server.listen(3000);
 
